c423e1d2a050e2a5f54ac1e221284c54d1911d0e
Uladzimir Kuzmich <s33258@pjwstk.edu.pl>
2026-03-17 20:21:30 +0100

    Add CalculateAverage method for integer arrays

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -9,4 +9,21 @@ public static class StatisticsHelper
     {
         return (a + b) / 2;
     }
+
+    public static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+
+        int sum = 0;
+
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+
+        return (double)sum / values.Length;
+    }
 }
