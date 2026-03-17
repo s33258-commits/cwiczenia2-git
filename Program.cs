ae2abf4b12768e93e0bff42d54f1e141b320a538
Uladzimir Kuzmich <s33258@pjwstk.edu.pl>
2026-03-17 20:22:16 +0100

    Display average calculated from integer array

--- Program.cs
+++ Program.cs
@@ -8,9 +8,11 @@ if (double.TryParse(input1, out double number1) && double.TryParse(input2, out d
 {
     double square1 = StatisticsHelper.Square(number1);
     double average = StatisticsHelper.Average(number1, number2);
+    double arrayAverage = StatisticsHelper.CalculateAverage(new int[] { (int)number1, (int)number2 });
 
     Console.WriteLine($"Kwadrat liczby {number1} = {square1}");
     Console.WriteLine($"Średnia liczb {number1} i {number2} = {average}");
+    Console.WriteLine($"Średnia z tablicy int[] = {arrayAverage}");
 }
 else
 {
