b8c8bed560b0cdc7d8977a685550290503d8ccf0
Uladzimir Kuzmich <s33258@pjwstk.edu.pl>
2026-03-17 20:34:27 +0100

    Display minimum value calculated from integer array

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ if (double.TryParse(input1, out double number1) && double.TryParse(input2, out d
     double square1 = StatisticsHelper.Square(number1);
     double average = StatisticsHelper.Average(number1, number2);
     double arrayAverage = StatisticsHelper.CalculateAverage(new int[] { (int)number1, (int)number2 });
+    int maxValue = StatisticsHelper.CalculateMax(new int[] { (int)number1, (int)number2 });
+    int minValue = StatisticsHelper.CalculateMin(new int[] { (int)number1, (int)number2 });
 
     Console.WriteLine($"Kwadrat liczby {number1} = {square1}");
     Console.WriteLine($"Średnia liczb {number1} i {number2} = {average}");
     Console.WriteLine($"Średnia z tablicy int[] = {arrayAverage}");
+    Console.WriteLine($"Maksimum z tablicy int[] = {maxValue}");
+    Console.WriteLine($"Minimum z tablicy int[] = {minValue}");
 }
 else
 {
