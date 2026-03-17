9afa0ac263ffb2d4602dbe3a9d4423151a3652b5
Uladzimir Kuzmich <s33258@pjwstk.edu.pl>
2026-03-17 19:59:18 +0100

    Add basic user input handling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Podaj liczbę: ");
+string? input = Console.ReadLine();
+
+double number = Convert.ToDouble(input);
+double result = StatisticsHelper.Square(number);
+
+Console.WriteLine($"Kwadrat liczby {number} = {result}");
