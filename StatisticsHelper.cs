36aca958b5105ecc4939173d22c0eb7db0d9709e
Uladzimir Kuzmich <s33258@pjwstk.edu.pl>
2026-03-17 20:25:24 +0100

    Add CalculateMax method for integer arrays

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -26,4 +26,24 @@ public static class StatisticsHelper
 
         return (double)sum / values.Length;
     }
+
+    public static int CalculateMax(int[] values)
+    {
+        if (values == null || values.Length == 0)
+        {
+            return 0;
+        }
+
+        int max = values[0];
+
+        foreach (int value in values)
+        {
+            if (value > max)
+            {
+                max = value;
+            }
+        }
+
+        return max;
+    }
 }
